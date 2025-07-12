11984335a327147860236e04a2e4e50023272f9b
Suraj Kumar <suraj06022003@gmail.com>
2025-07-12 16:55:18 +0530

    Refactor App and index CSS; remove unused imports and enhance layout structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import { FaArrowRight } from "react-icons/fa6";
-import HambergerMenu from "./components/HambergerMenu";
+import { FaArrowRight, FaFileCode, FaLaptopCode } from "react-icons/fa6";
 import { useState } from "react";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { FaHandSparkles } from "react-icons/fa";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +12,7 @@ function App() {
       {/* Navbar */}
       <nav className="p-3 flex bg-white justify-between items-center">
         {/* Logo */}
-        <a href="/#" className="flex gap-2 items-center">
+        <a href="/#" className="flex gap-2 items-center flex-1">
           <img
             src="assets/asset 0.png"
             alt="Logo"
@@ -21,7 +21,7 @@ function App() {
           <span className="text-lg font-medium font-display ">ToDesktop</span>
         </a>
 
-        <div className="flex gap-12 hidden lg:flex">
+        <div className="gap-12 hidden lg:flex">
           <a href="/#" className="font-medium hover:text-primary">
             Pricing
           </a>
@@ -39,11 +39,13 @@ function App() {
           </a>
         </div>
 
-        <button className="hidden lg:flex gap-2 items-center border border-gray-300 px-6 py-2 rounded-lg hover:border-gray-500">
-          <img src="assets/asset 1.svg" alt="icon"></img>
-          <span>Electron Developers</span>
-          <FaArrowRight />
-        </button>
+        <div className="lg:flex hidden flex-1 justify-end">
+          <button className="flex gap-2 items-center border border-gray-300 px-6 py-2 rounded-lg hover:border-gray-500">
+            <img src="assets/asset 1.svg" alt="icon"></img>
+            <span>Electron Developers</span>
+            <FaArrowRight />
+          </button>
+        </div>
 
         {/* Hamburger Menu Toggle */}
         <button className="lg:hidden mr-0 p-2" onClick={() => setIsOpen(true)}>
@@ -92,13 +94,38 @@ function App() {
       {/* Main body */}
       <main>
         {/* Hero section */}
-        <div id="hero" className="min-h-screen bg-gradient-to-br from-purple-100 via-orange-100 to-transparent">
-          <div id="hero-container" className="max-w-4xl mx-auto px-6 pb-32 flex flex-col">
+        <div id="hero" className="min-h-screen bg-gradient-to-b from-purple-50 via-orange-50 to-transparent">
+          <div id="hero-container" className="max-w-4xl mx-auto px-6 pb-32 pt-5 flex flex-col sm:items-center sm:text-center sm:pt-12">
             {/* Version text */}
-            <div id="version-text">
-              
+            <div id="version-text" className="flex items-center my-6 gap-2 border border-yellow-300 bg-yellow-50 rounded-lg px-3 py-1 w-fit shadow-md hover:shadow-lg hover:-translate-y-1 transition group">
+              <div className="w-2 h-2 bg-yellow-400 rounded-full border border-yellow-600"></div>
+                <p className="font-display font-medium text-yellow-600">v0.21.1:<span className="text-yellow-800">Find-in-page bug fixes</span></p>
+                <FaArrowRight className="text-yellow-600 group-hover:translate-x-1 transition duration-300"/>
             </div>
 
+            {/* Hero features */}
+            <div className="hidden sm:flex gap-8 my-6">
+              <div className="flex justify-center gap-2 items-center text-gray-500">
+                <FaFileCode/>
+                <p>Code optional</p>
+              </div>
+              <div className="flex justify-center gap-2 items-center text-gray-500">
+                <FaHandSparkles/>
+                <p>Drag & drop builder</p>
+              </div>
+              <div className="flex justify-center gap-2 items-center text-gray-500">
+                <FaLaptopCode/>
+                <p>Windows, Mac, Linux</p>
+              </div>
+            </div>
+
+            <h1 className="text-4xl sm:text-7xl font-display font-semibold leading-snug sm:leading-normal mt-4">Web app to desktop app in minutes</h1>
+            <p className="text-xl sm:text-2xl mt-4 sm:mt-8 text-gray-800">Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
+
+            <div id="buttons-container" className="mt-12 flex gap-4 flex-col sm:flex-row">
+              <button className="px-8 py-3 font-semibold rounded-lg text-white bg-primary shadow-sm hover:opacity-90">Download now</button>
+              <button className="px-8 py-3 font-semibold rounded-lg bg-white border border-gray-400 hover:border-gray-800">Read Docs</button>
+            </div>
           </div>
         </div>
       </main>
